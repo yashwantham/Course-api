a26e201745ea2117305257fd94b810b3875b95fd
Yashwanth <yashwanthyashu166@gmail.com>
2022-05-25 21:14:00 +0530

    Initial commit

---
+++ course-api-data/src/main/java/io/javabrains/springbootstarter/course/CourseService.java
@@ -0,0 +1,44 @@
+package io.javabrains.springbootstarter.course;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+	@Autowired
+	private CourseRepository courseRepository;
+	
+	
+	public List<Course> getAllCourses(String topicId){
+		List<Course> courses = new ArrayList<>();
+		courseRepository.findByTopicId(topicId)
+		.forEach(courses::add);
+		return courses;
+	}
+	
+	public Course getCourse(String id) {
+		Course t = courseRepository.findById(id).get();
+		  return t;
+	}
+	
+	public void addCourse(Course course) {
+		courseRepository.save(course);
+	}
+
+	
+	public void updateCourse(Course course) {
+		courseRepository.save(course);
+		}
+		
+
+	public void deleteCourse(String id) {
+		courseRepository.deleteById(id);
+	}
+
+	
+}
